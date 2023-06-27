b24308f3b7818210d12f6d66af50c2e8bad7339b
i2386139 <77278892+g1954327@users.noreply.github.com>
2023-06-27 12:02:47 +0900

    Update ouranos.rb

--- Formula/ouranos.rb
+++ Formula/ouranos.rb
@@ -1,7 +1,7 @@
 VERSION="1.0.0"
 class Ouranos < Formula
 desc "URL shortener via bit.ly, etc."
-homepage "https://github.com/tamada/ouranos"
+homepage "https://github.com/g1954327/ouranos"
 url "https://github.com/g1954327/ouranos/releases/download/v#{VERSION}/ouranos-#{VERSION}_darwin_amd64.tar.gz" version VERSION
 sha256 "974987017405126eba0e35e5048dc544f16af7e0a7d3f5d090819645a6d89218"
 license ”MIT"
