42d3bb354dea5653e278d4fa8d49e2b89318dc01
i2386139 <77278892+g1954327@users.noreply.github.com>
2023-07-13 14:17:30 +0900

    Update ouranos.rb

--- Formula/ouranos.rb
+++ Formula/ouranos.rb
@@ -2,14 +2,15 @@ VERSION="0.1.16"
 class Ouranos < Formula
 desc "URL shortener via bit.ly, etc."
 homepage "https://github.com/g1954327/ouranos"
-url "https://github.com/g1954327/ouranos/releases/download/v#{VERSION}/ouranos-#{VERSION}_darwin_amd64.tar.gz" version VERSION
+url "https://github.com/g1954327/ouranos/releases/download/v#{VERSION}/ouranos-#{VERSION}_darwin_amd64.tar.gz" 
+version VERSION
 sha256 "974987017405126eba0e35e5048dc544f16af7e0a7d3f5d090819645a6d89218"
-license ”MIT"
+license "MIT"
 option "without-completions", "Disable bash completions"
 depends_on "bash-completion@2" => :optional
 def install
-    bin.install ”ouranos"
+    bin.install "ouranos"
 bash_completion.install "completions/bash/ouranos" if build.with? "completions" end
   test do
-    system bin/”ouranos", "--version"
+    system bin/"ouranos", "--version"
 end end
