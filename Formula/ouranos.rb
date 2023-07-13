2c762adf9409f2349426856013c642785356c9a2
i2386139 <77278892+g1954327@users.noreply.github.com>
2023-07-13 14:41:26 +0900

    ouranos.rbのsha256の値を変更した

--- Formula/ouranos.rb
+++ Formula/ouranos.rb
@@ -1,10 +1,11 @@
 VERSION="0.1.16"
+
 class Ouranos < Formula
 desc "URL shortener via bit.ly, etc."
 homepage "https://github.com/g1954327/ouranos"
 url "https://github.com/g1954327/ouranos/releases/download/v#{VERSION}/ouranos-#{VERSION}_darwin_amd64.tar.gz" 
 version VERSION
-sha256 "974987017405126eba0e35e5048dc544f16af7e0a7d3f5d090819645a6d89218"
+sha256 "392dd0ec2360706e486f84d8fbe34096fc1b15b2a95d2503473d5fdce6640de1"
 license "MIT"
 option "without-completions", "Disable bash completions"
 depends_on "bash-completion@2" => :optional
